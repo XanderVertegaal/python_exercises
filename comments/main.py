a7de2df767f3316c91e12226e313df38444dd72e
Xander Vertegaal <xander.vertegaal@xs4all.nl>
2021-11-24 12:05:59 +0100

    String manipulation

---
+++ comments/main.py
@@ -0,0 +1,19 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+"""
+Here my script starts
+"""
+
+# Initialises a new string
+my_message = 'Welcome to this website.' # Variable is initialised
+
+# Prints my message
+print(my_message)   # Message is printed
+
+"""
+Here my script ends
+"""
